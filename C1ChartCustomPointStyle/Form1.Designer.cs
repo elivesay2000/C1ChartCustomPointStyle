d9b0cd87d7d3e210e6125f269c1a3e9e417c3030
Eric Livesay <eric.livesay@hecate.com>
2020-02-17 16:36:03 -0800

    Add project files.

---
+++ C1ChartCustomPointStyle/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace C1ChartCustomPointStyle
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+         this.c1Chart1 = new C1.Win.C1Chart.C1Chart();
+         this.cmdStart = new System.Windows.Forms.Button();
+         ((System.ComponentModel.ISupportInitialize)(this.c1Chart1)).BeginInit();
+         this.SuspendLayout();
+         // 
+         // c1Chart1
+         // 
+         this.c1Chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+         this.c1Chart1.Location = new System.Drawing.Point(12, 12);
+         this.c1Chart1.Name = "c1Chart1";
+         this.c1Chart1.PropBag = resources.GetString("c1Chart1.PropBag");
+         this.c1Chart1.Size = new System.Drawing.Size(776, 385);
+         this.c1Chart1.TabIndex = 0;
+         // 
+         // cmdStart
+         // 
+         this.cmdStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+         this.cmdStart.Location = new System.Drawing.Point(12, 415);
+         this.cmdStart.Name = "cmdStart";
+         this.cmdStart.Size = new System.Drawing.Size(75, 23);
+         this.cmdStart.TabIndex = 1;
+         this.cmdStart.Text = "Start";
+         this.cmdStart.UseVisualStyleBackColor = true;
+         this.cmdStart.Click += new System.EventHandler(this.cmdStart_Click);
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(800, 450);
+         this.Controls.Add(this.cmdStart);
+         this.Controls.Add(this.c1Chart1);
+         this.Name = "Form1";
+         this.Text = "Form1";
+         ((System.ComponentModel.ISupportInitialize)(this.c1Chart1)).EndInit();
+         this.ResumeLayout(false);
+
+      }
+
+        #endregion
+
+        private C1.Win.C1Chart.C1Chart c1Chart1;
+        private System.Windows.Forms.Button cmdStart;
+    }
+}
+
---
+++ C1ChartCustomPointStyle/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace C1ChartCustomPointStyle
+{
+   public partial class Form1 : Form
+   {
+      private Timer                 TimerStart           = null;
+      private Random                random               = new Random();
+      private int                   upper                = 1100;
+      private int                   upperBoundary        = 1050;
+      private int                   lowerBoundary        =  950;
+      private int                   lower                =  900;
+
+      private const string         mainDataLabel         = "Test 1 2 3";
+      private const string    lowerBoundaryLabel         = "Lower Boundary";
+      private const string    upperBoundaryLabel         = "Upper Boundary";
+
+      public Form1()
+      {
+         InitializeComponent();
+
+         C1.Win.C1Chart.PointStyle ps   = new C1.Win.C1Chart.PointStyle();
+
+         ps.Selection                   = C1.Win.C1Chart.PointStyleSelectionEnum.Custom;
+         ps.Select                     += Ps_Select;
+
+         // This is only being done on the left-hand y-axis.
+         c1Chart1.ChartGroups[0].ChartData.PointStylesList.Add(ps);
+      }
+
+		private void				Ps_Select							(object sender, C1.Win.C1Chart.PointStyleSelectEventArgs e)
+		{
+			C1.Win.C1Chart.PointStyle				ps = sender as C1.Win.C1Chart.PointStyle;
+			C1.Win.C1Chart.ChartDataSeries		ds = c1Chart1.ChartGroups[0].ChartData[e.SeriesIndex];
+
+			int y = Convert.ToInt32(ds.Y[e.PointIndex]);
+
+         Console.WriteLine("y = " + y.ToString());
+
+			if			(y > upperBoundary)  ps.SymbolStyle.Color = Color.Red;
+         else if	(y < lowerBoundary)  ps.SymbolStyle.Color = Color.Blue;
+			else						         ps.SymbolStyle.Color = Color.Black;
+
+			e.Selected = true;
+		}
+
+      private void      plot(string seriesLabel, DateTime theTime, int theData)
+      {
+         //---------------
+         // Get the series
+         //---------------
+         C1.Win.C1Chart.ChartDataSeries theSeries = null;
+         foreach (C1.Win.C1Chart.ChartDataSeries s in c1Chart1.ChartGroups[0].ChartData.SeriesList)
+         {
+            if (s.Label == seriesLabel)
+            {
+               theSeries = s;
+               break;
+            }
+         }
+         if(theSeries == null)
+         { 
+            theSeries = c1Chart1.ChartGroups[0].ChartData.SeriesList.AddNewSeries();
+            switch(seriesLabel)
+            {
+               case mainDataLabel:        theSeries.SymbolStyle.Shape   = C1.Win.C1Chart.SymbolShapeEnum.Dot;
+                                          theSeries.SymbolStyle.Color   = Color.Black;
+                                          theSeries.LineStyle.Pattern   = C1.Win.C1Chart.LinePatternEnum.None;
+                                          theSeries.LineStyle.Color     = Color.Black;
+                                          break;
+
+               case lowerBoundaryLabel:   theSeries.SymbolStyle.Shape   = C1.Win.C1Chart.SymbolShapeEnum.None;
+                                          theSeries.SymbolStyle.Color   = Color.Transparent;
+                                          theSeries.LineStyle.Pattern   = C1.Win.C1Chart.LinePatternEnum.Dash;
+                                          theSeries.LineStyle.Color     = Color.Blue;
+                                          break;
+
+               case upperBoundaryLabel:   theSeries.SymbolStyle.Shape   = C1.Win.C1Chart.SymbolShapeEnum.None;
+                                          theSeries.SymbolStyle.Color   = Color.Transparent;
+                                          theSeries.LineStyle.Pattern   = C1.Win.C1Chart.LinePatternEnum.Dash;
+                                          theSeries.LineStyle.Color     = Color.Red;
+                                          break;
+            }
+            theSeries.Label = seriesLabel;
+
+         }
+
+         // Plot the data
+         Console.WriteLine("X = " + theTime.ToString() + ", Y = " + theData.ToString());
+         theSeries.X.Add(theTime );
+         theSeries.Y.Add(theData );
+
+         // Only show 60 seconds of data
+         while (theSeries.PointData.Length > 60)
+         {
+            theSeries.X.RemoveAt(0);
+            theSeries.Y.RemoveAt(0);
+         }
+
+      }
+
+      private void      TimerStart_Tick(object sender, EventArgs e)
+      {
+         TimerStart.Enabled = false;
+
+         // Plot the data
+         DateTime theTime = new DateTime  (DateTime.Now.Ticks);
+         int      theData = random.Next   (lower, upper);
+
+         plot(mainDataLabel,        theTime, theData        );
+         plot(lowerBoundaryLabel,   theTime, lowerBoundary  );
+         plot(upperBoundaryLabel,   theTime, upperBoundary  );
+
+         TimerStart.Enabled = true;
+      }
+
+      private void cmdStart_Click(object sender, EventArgs e)
+      {
+         c1Chart1.ChartGroups[0].ChartData.SeriesList.Clear();
+
+         TimerStart           = new Timer();
+         TimerStart.Interval  = 1000;
+         TimerStart.Tick     += TimerStart_Tick;
+         TimerStart.Enabled   = true;
+      }
+   }
+}
